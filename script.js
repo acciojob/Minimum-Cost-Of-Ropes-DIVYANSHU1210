3cc7bcad010e400fdc0bd4e053be02501449f39d
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-19 00:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function calculateMinCost() {
+function calculateMinCost(arr) {
   //your code here
-  
+	let sum = 0;
+  while(arr.length > 1){
+	  arr.sort((a,b) => a-b);
+	  let p = arr.shift();
+	  let q = arr.shift();
+	  sum += p+q;
+	  arr.push(p+q);
+  }
+	return sum;
   
   
 }  
