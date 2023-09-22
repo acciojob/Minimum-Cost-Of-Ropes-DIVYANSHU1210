95b55be6022d59437fcafd2af9e7718088232e8e
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-22 18:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-function calculateMinCost(arr) {
+function calculateMinCost() {
   //your code here
-	let sum = 0;
-  while(arr.length > 1){
-	  arr.sort((a,b) => a-b);
-	  let p = arr.shift();
-	  let q = arr.shift();
-	  sum += p+q;
-	  arr.push(p+q);
-  }
-	return sum;
-  
+	const arr = document.getElementById("rope-lengths");
+	const result = document.getElementById("result");
+	
+	const ar = arr.value.split(",").map(Number);
+    let sum = 0;
+	
+    while (ar.length > 1) {
+        ar.sort((a, b) => a - b);
+        let p = ar.shift();
+        let q = ar.shift();
+        sum += p + q;
+        ar.push(p + q);
+    }
+
+    // Display the result in the result div
+    result.textContent = `${sum}`;
   
 }  
+
+
+// function calculateMinCost(arr) {
+//     const ropeLengthsInput = document.getElementById("rope-lengths");
+//     const result = document.getElementById("result");
+
+//     // Get the input value and split it into an array of integers
+//     const inputValues = ropeLengthsInput.value.split(",").map(Number);
+
+//     // Check if the input is valid
+//     if (inputValues.some(isNaN)) {
+//         result.textContent = "Invalid input. Please enter comma-separated integers.";
+//         return;
+//     }
+
+//     // Calculate the minimum cost using your algorithm
+//     let sum = 0;
+//     while (inputValues.length > 1) {
+//         inputValues.sort((a, b) => a - b);
+//         let p = inputValues.shift();
+//         let q = inputValues.shift();
+//         sum += p + q;
+//         inputValues.push(p + q);
+//     }
+
+//     // Display the result in the result div
+//     result.textContent = `Minimum cost of ropes: ${sum}`;
+// }
